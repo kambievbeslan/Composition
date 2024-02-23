d3edc000caa6fd1bff6ec7e858a9d712cc170765
Beslan Kambiev <kambiev.b.m@gmail.com>
2024-02-23 20:00:17 +0300

    Serialization

--- app/src/main/java/com/beslan/composition/presentation/GameFinishedFragment.kt
+++ app/src/main/java/com/beslan/composition/presentation/GameFinishedFragment.kt
@@ -39,6 +39,9 @@ class GameFinishedFragment : Fragment(){
             }
         }
         requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner, callback)
+        binding.buttonRetry.setOnClickListener {
+            retryGame()
+        }
     }
     override fun onDestroyView() {
         super.onDestroyView()
@@ -46,7 +49,9 @@ class GameFinishedFragment : Fragment(){
     }
 
     private fun parseArgs() {
-        gameResult = requireArguments().getSerializable(KEY_GAME_RESULT) as GameResult
+        requireArguments().getParcelable<GameResult>(KEY_GAME_RESULT)?.let {
+            gameResult = it
+        }
     }
 
     private fun retryGame() {
@@ -60,7 +65,7 @@ class GameFinishedFragment : Fragment(){
         fun newInstance(gameResult: GameResult): GameFinishedFragment {
             return GameFinishedFragment().apply {
                 arguments = Bundle().apply {
-                    putSerializable(KEY_GAME_RESULT, gameResult)
+                    putParcelable(KEY_GAME_RESULT, gameResult)
                 }
             }
         }
