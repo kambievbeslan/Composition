10959b1d566705f1e86ac6ea5629acf35d2b3e80
Beslan Kambiev <kambiev.b.m@gmail.com>
2024-02-23 15:32:49 +0300

    Domain

---
+++ app/src/main/java/com/beslan/composition/domain/entity/GameSettings.kt
@@ -0,0 +1,8 @@
+package com.beslan.composition.domain.entity
+
+data class GameSettings(
+    val maxSumValue: Int,
+    val minCountOfRightAnswers: Int,
+    val minPercentOfRightAnswers: Int,
+    val gamTimeInSeconds: Int
+)
--- app/src/main/java/com/beslan/composition/presentation/MainActivity.kt
+++ app/src/main/java/com/beslan/composition/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.beslan.composition
+package com.beslan.composition.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.beslan.composition.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
