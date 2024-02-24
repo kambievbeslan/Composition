142f7780935001e0e4b66fdd29290a39e5da58b9
Beslan Kambiev <kambiev.b.m@gmail.com>
2024-02-24 12:18:02 +0300

    ViewModelFactory

--- app/src/main/java/com/beslan/composition/presentation/GameFinishedFragment.kt
+++ app/src/main/java/com/beslan/composition/presentation/GameFinishedFragment.kt
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
+import com.beslan.composition.R
 import com.beslan.composition.databinding.FragmentGameFinishedBinding
 import com.beslan.composition.domain.entity.GameResult
 
@@ -33,6 +34,11 @@ class GameFinishedFragment : Fragment(){
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        setupClickListeners()
+        bindViews()
+    }
+
+    private fun setupClickListeners() {
         val callback = object : OnBackPressedCallback(true){
             override fun handleOnBackPressed() {
                 retryGame()
@@ -43,6 +49,45 @@ class GameFinishedFragment : Fragment(){
             retryGame()
         }
     }
+
+    private fun bindViews() {
+        with(binding) {
+            emojiResult.setImageResource(getSmileResId())
+            tvRequiredAnswers.text = String.format(
+                getString(R.string.required_score),
+                gameResult.gameSettings.minCountOfRightAnswers
+            )
+            tvScoreAnswers.text = String.format(
+                getString(R.string.score_answers),
+                gameResult.countOfRightAnswers
+            )
+            tvRequiredPercentage.text = String.format(
+                getString(R.string.required_percentage),
+                gameResult.gameSettings.minPercentOfRightAnswers
+            )
+            tvScorePercentage.text = String.format(
+                getString(R.string.score_percentage),
+                getPercentOfRightAnswers()
+            )
+        }
+    }
+
+    private fun getSmileResId(): Int {
+        return if (gameResult.winner) {
+            R.drawable.ic_smile
+        } else {
+            R.drawable.ic_sad
+        }
+    }
+    private fun getPercentOfRightAnswers(): Int =
+        with(gameResult) {
+            if (countOfQuestions == 0) {
+                0
+            } else {
+                (countOfRightAnswers / countOfQuestions * 100).toInt()
+            }
+        }
+
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
--- app/src/main/java/com/beslan/composition/presentation/GameViewModel.kt
+++ app/src/main/java/com/beslan/composition/presentation/GameViewModel.kt
@@ -1,10 +1,12 @@
 package com.beslan.composition.presentation
 
 import android.app.Application
+import android.content.Context
 import android.os.CountDownTimer
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
 import com.beslan.composition.R
 import com.beslan.composition.data.GameRepositoryImpl
 import com.beslan.composition.domain.entity.GameResult
@@ -14,12 +16,12 @@ import com.beslan.composition.domain.entity.Question
 import com.beslan.composition.domain.usecases.GenerateQuestionUseCase
 import com.beslan.composition.domain.usecases.GetGameSettingsUseCase
 
-class GameViewModel(application: Application) : AndroidViewModel(application) {
+class GameViewModel(
+    private val application: Application,
+    private val level: Level
+) : ViewModel() {
 
     private lateinit var gameSettings: GameSettings
-    private lateinit var level: Level
-
-    private val context = application
 
     private val repository = GameRepositoryImpl
 
@@ -63,10 +65,15 @@ class GameViewModel(application: Application) : AndroidViewModel(application) {
     private var countOfRightAnswers = 0
     private var countOfQuestions = 0
 
-    fun startGame(level: Level) {
-        getGameSettings(level)
+    init {
+        startGame()
+    }
+
+    private fun startGame() {
+        getGameSettings()
         startTimer()
         generateQuestion()
+        updateProgress()
     }
 
     fun chooseAnswer(number: Int) {
@@ -79,7 +86,7 @@ class GameViewModel(application: Application) : AndroidViewModel(application) {
         val percent = calculatePercentOfRightAnswers()
         _percentOfRightAnswers.value = percent
         _progressAnswers.value = String.format(
-            context.resources.getString(R.string.progress_answers),
+            application.resources.getString(R.string.progress_answers),
             countOfRightAnswers,
             gameSettings.minCountOfRightAnswers
         )
@@ -88,6 +95,9 @@ class GameViewModel(application: Application) : AndroidViewModel(application) {
     }
 
     private fun calculatePercentOfRightAnswers(): Int {
+        if (countOfQuestions == 0) {
+            return 0
+        }
         return (countOfRightAnswers / countOfQuestions.toDouble() * 100).toInt()
     }
 
@@ -99,8 +109,7 @@ class GameViewModel(application: Application) : AndroidViewModel(application) {
         countOfQuestions++
     }
 
-    private fun getGameSettings(level: Level) {
-        this.level = level
+    private fun getGameSettings() {
         this.gameSettings = getGameSettingsUseCase(level)
         _minPercent.value = gameSettings.minPercentOfRightAnswers
     }
