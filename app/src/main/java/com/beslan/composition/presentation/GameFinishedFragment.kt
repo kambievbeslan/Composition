9df48de16059ab861960e0571cf19174d2d76718
Beslan Kambiev <kambiev.b.m@gmail.com>
2024-02-24 16:36:25 +0300

    DataBinding

--- app/src/main/java/com/beslan/composition/presentation/GameFinishedFragment.kt
+++ app/src/main/java/com/beslan/composition/presentation/GameFinishedFragment.kt
@@ -4,14 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentManager
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
-import com.beslan.composition.R
 import com.beslan.composition.databinding.FragmentGameFinishedBinding
-import com.beslan.composition.domain.entity.GameResult
 
 class GameFinishedFragment : Fragment(){
 
@@ -33,7 +29,7 @@ class GameFinishedFragment : Fragment(){
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setupClickListeners()
-        bindViews()
+        binding.gameResult = args.gameResult
     }
 
     private fun setupClickListeners() {
@@ -41,44 +37,6 @@ class GameFinishedFragment : Fragment(){
             retryGame()
         }
     }
-
-    private fun bindViews() {
-        with(binding) {
-            emojiResult.setImageResource(getSmileResId())
-            tvRequiredAnswers.text = String.format(
-                getString(R.string.required_score),
-                args.gameResult.gameSettings.minCountOfRightAnswers
-            )
-            tvScoreAnswers.text = String.format(
-                getString(R.string.score_answers),
-                args.gameResult.countOfRightAnswers
-            )
-            tvRequiredPercentage.text = String.format(
-                getString(R.string.required_percentage),
-                args.gameResult.gameSettings.minPercentOfRightAnswers
-            )
-            tvScorePercentage.text = String.format(
-                getString(R.string.score_percentage),
-                getPercentOfRightAnswers()
-            )
-        }
-    }
-
-    private fun getSmileResId(): Int {
-        return if (args.gameResult.winner) {
-            R.drawable.ic_smile
-        } else {
-            R.drawable.ic_sad
-        }
-    }
-    private fun getPercentOfRightAnswers(): Int =
-        with(args.gameResult) {
-            if (countOfQuestions == 0) {
-                0
-            } else {
-                (countOfRightAnswers / countOfQuestions * 100).toInt()
-            }
-        }
 
     override fun onDestroyView() {
         super.onDestroyView()
